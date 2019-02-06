0ada3481739d7e926669cb46e5c682f0b23e8026
HananeIder <46898959+HananeIder@users.noreply.github.com>
2019-02-07 00:01:50 +0100

    Update cv.js

--- cv.js
+++ cv.js
@@ -6,13 +6,13 @@ window.addEventListener("scroll", function() {
 // Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
 
 	document.getElementById('progress_bar').value = deplacement;
-
+	
 // Affiche le contenu de la variable maVariable dans la console du navigateur 
 	console.log(deplacement);
 });
 // Définition de la fonction de scroll
 function myScroll() { 
-
+	
 // Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
 	var progress = document.getElementById('progress_bar').value;
 // valeur de progress de la bare qui permet de changer et modifier la page avec scroll	
